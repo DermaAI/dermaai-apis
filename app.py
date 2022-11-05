f9b82da9a09c068e3340f1359f333162cba7888d
anasalansari <77924312+anas-ansri@users.noreply.github.com>
2022-11-05 19:47:31 +0530

    update

--- app.py
+++ app.py
@@ -7,29 +7,20 @@ from io import BytesIO
 # load api
 app = FastAPI()
 
-# load model for prediction
-# Load the TFLite model and allocate tensors.
-interpreter1 = tf.lite.Interpreter(model_path="models/main-model1.tflite")
-interpreter1.allocate_tensors()
 
-# Get input and output tensors.
-input_details1 = interpreter1.get_input_details()
-output_details1 = interpreter1.get_output_details()
-
-# load model for prediction
-# Load the TFLite model and allocate tensors.
-interpreter2 = tf.lite.Interpreter(model_path="models/second-model.tflite")
-interpreter2.allocate_tensors()
-
-# Get input and output tensors.
-input_details2 = interpreter2.get_input_details()
-output_details2 = interpreter2.get_output_details()
-
-# method to predict main disease
 def mainPredict(image: Image.Image):
     image = np.asarray(image.resize((224, 224)), dtype=np.float32)[..., :3]
     image = np.expand_dims(image, 0)
     images = np.vstack([image])
+
+    # load model for prediction
+    # Load the TFLite model and allocate tensors.
+    interpreter1 = tf.lite.Interpreter(model_path="models/main-model1.tflite")
+    interpreter1.allocate_tensors()
+
+    # Get input and output tensors.
+    input_details1 = interpreter1.get_input_details()
+    output_details1 = interpreter1.get_output_details()
 
     # # Test the model on random input data.
     input_shape = input_details1[0]['shape']
@@ -37,17 +28,30 @@ def mainPredict(image: Image.Image):
     interpreter1.invoke()
     # The function `get_tensor()` returns a copy of the tensor data.
     # Use `tensor()` in order to get a pointer to the tensor.
-    output_data = interpreter1.get_tensor(output_details1[0]['index'])    
+    output_data = interpreter1.get_tensor(output_details1[0]['index'])
     return {
         "prediction": str(np.argmax(output_data)),
         "all": str(output_data)
     }
 
-# method to predict cancer 
+# method to predict cancer
+
+
 def cancerPredict(image: Image.Image):
     image = np.asarray(image.resize((224, 224)), dtype=np.float32)[..., :3]
     image = np.expand_dims(image, 0)
     images = np.vstack([image])
+
+    # load model for prediction
+    # Load the TFLite model and allocate tensors.
+    interpreter2 = tf.lite.Interpreter(model_path="models/second-model.tflite")
+    interpreter2.allocate_tensors()
+
+    # Get input and output tensors.
+    input_details2 = interpreter2.get_input_details()
+    output_details2 = interpreter2.get_output_details()
+
+    # method to predict main disease
 
     # # Test the model on random input data.
     input_shape = input_details2[0]['shape']
@@ -55,12 +59,11 @@ def cancerPredict(image: Image.Image):
     interpreter2.invoke()
     # The function `get_tensor()` returns a copy of the tensor data.
     # Use `tensor()` in order to get a pointer to the tensor.
-    output_data = interpreter2.get_tensor(output_details2[0]['index'])    
+    output_data = interpreter2.get_tensor(output_details2[0]['index'])
     return {
         "prediction": str(np.argmax(output_data)),
         "all": str(output_data)
     }
-
 
 
 # sample home page
@@ -68,11 +71,14 @@ def cancerPredict(image: Image.Image):
 def index():
     return {'message': 'Hello, World'}
 
+
 def read_imagefile(file) -> Image.Image:
     image = Image.open(BytesIO(file))
     return image
 
 # main model prediction
+
+
 @app.post("/predict/main")
 async def predict_api(file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
@@ -83,6 +89,8 @@ async def predict_api(file: UploadFile = File(...)):
     return prediction
 
 # cancer model prediction
+
+
 @app.post("/predict/cancer")
 async def predict_api(file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
@@ -97,5 +105,3 @@ if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
 
 # uvicorn app:app --reload
-
-
