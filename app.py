5a818ae6b7b3140eed0fca78bc3b03c95d95c188
Anas Ansari <77924312+anas-ansri@users.noreply.github.com>
2022-11-05 19:33:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 import numpy as np
-import tensorflow as tf
+import tensorflow_cpu as tf
 from PIL import Image
 from io import BytesIO
 # load api
