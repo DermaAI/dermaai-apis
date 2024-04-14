df1c3e3f417beb2ac37cd6e3cb2213725837c88e
Anas Ansari <77924312+anas-ansri@users.noreply.github.com>
2024-04-14 13:38:56 +0530

    build(deps): delete 2 files and update 1 file

--- main.py
+++ main.py
@@ -102,6 +102,6 @@ async def predict_api(file: UploadFile = File(...)):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=80)
 
-# uvicorn app:app --reload
+# uvicorn main:app --reload
